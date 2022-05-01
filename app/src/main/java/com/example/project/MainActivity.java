4c841751eccb4443d779d21265d7720b73c771c2
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-01 13:52:13 +0200

    Add inflater and handler for menu item "Show about page"

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -15,4 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_about) {
+            Log.d("MainActivity", String.format("Clicked: %s", item));
+            //TODO: Show about page.
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
