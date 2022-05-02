6a8e39767018c8b7e80c82fea293f60dba9560f4
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-02 15:55:38 +0200

    Add internal html document and WebView to show it

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -3,12 +3,18 @@ package com.example.project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private static final String filePath = "file:///android_asset/about.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        WebView view = findViewById(R.id.web_view);
+        view.loadUrl(filePath);
     }
 }
