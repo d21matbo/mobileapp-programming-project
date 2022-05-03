a24a40a0b51fd3d03154e72644b65d6bf6293082
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-03 09:37:34 +0200

    Add basic Adapter implementation

--- app/src/main/java/com/example/project/ProjectAdapter.java
+++ app/src/main/java/com/example/project/ProjectAdapter.java
@@ -1,24 +1,36 @@
 package com.example.project;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class ProjectAdapter extends RecyclerView.Adapter<ProjectViewHolder> {
+
+    //private List<Employee> employees = new ArrayList<Employee>();
+
+    public ProjectAdapter() {
+    }
+
     @NonNull
     @Override
     public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new ProjectViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
-
+        //holder.variable.setText, Set all values for the list_item
     }
 
     @Override
     public int getItemCount() {
         return 0;
+        //return employees.size();
     }
 }
--- app/src/main/java/com/example/project/ProjectViewHolder.java
+++ app/src/main/java/com/example/project/ProjectViewHolder.java
@@ -6,7 +6,10 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ProjectViewHolder extends RecyclerView.ViewHolder {
+    //Add views from the list_item.xml
+
     public ProjectViewHolder(@NonNull View itemView) {
         super(itemView);
+        //variable = itemView.findViewById(R.id.)
     }
 }
