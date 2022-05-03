43bb2e55491a94d41d1ec214e5a5b0d74c9b261a
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-03 20:56:52 +0200

    Add class Employee.java with some of attributes

---
+++ app/src/main/java/com/example/project/Employee.java
@@ -0,0 +1,19 @@
+package com.example.project;
+
+public class Employee {
+    private String name;
+    private String position;
+
+    public Employee(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+}
