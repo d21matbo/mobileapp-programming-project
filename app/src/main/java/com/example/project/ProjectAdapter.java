f35ff9bf0e51299f22d724130508b62df881b948
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-03 09:17:17 +0200

    Add template code for Adapter and ViewHolder

---
+++ app/src/main/java/com/example/project/ProjectAdapter.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProjectAdapter extends RecyclerView.Adapter<ProjectViewHolder> {
+    @NonNull
+    @Override
+    public ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/project/ProjectViewHolder.java
@@ -0,0 +1,12 @@
+package com.example.project;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ProjectViewHolder extends RecyclerView.ViewHolder {
+    public ProjectViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
