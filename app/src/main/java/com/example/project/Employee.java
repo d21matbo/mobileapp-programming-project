bd92fd6f611821722c9ac52464f51d0754640ac0
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-05 20:57:59 +0200

    Add new constructor and missing getters

--- app/src/main/java/com/example/project/Employee.java
+++ app/src/main/java/com/example/project/Employee.java
@@ -1,15 +1,26 @@
 package com.example.project;
 
-    public class Employee {
+public class Employee {
     private String name;
     private String position;
     private int id;
-    //private Date dob;
+    private DoB dob;
     private int age; //Derived
     private String location;
     private int number;
     private String mail;
 
+    public Employee(String name, String position, int id, DoB dob, String location, int number, String mail) {
+        this.name = name;
+        this.position = position;
+        this.id = id;
+        this.dob = dob;
+        this.location = location;
+        this.number = number;
+        this.mail = mail;
+
+        age = dob.getCurrentAge();
+    }
 
     public Employee(String name, String position) {
         this.name = name;
@@ -22,5 +33,29 @@ package com.example.project;
 
     public String getPosition() {
         return position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDob() {
+        return dob.getDoB();
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getMail() {
+        return mail;
     }
 }
