8ef4da6b6cdaa2fb1ed2c4e5239196429f881ce4
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-05 08:54:13 +0200

    Add more member variables

--- app/src/main/java/com/example/project/Employee.java
+++ app/src/main/java/com/example/project/Employee.java
@@ -1,8 +1,15 @@
 package com.example.project;
 
-public class Employee {
+    public class Employee {
     private String name;
     private String position;
+    private int id;
+    //private Date dob;
+    private int age; //Derived
+    private String location;
+    private int number;
+    private String mail;
+
 
     public Employee(String name, String position) {
         this.name = name;
