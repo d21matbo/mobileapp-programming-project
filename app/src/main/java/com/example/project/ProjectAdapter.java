9d19679972f1fedef83180b639285bb50363007c
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-13 14:39:31 +0200

    Clean up code

--- app/src/main/java/com/example/project/ProjectAdapter.java
+++ app/src/main/java/com/example/project/ProjectAdapter.java
@@ -1,6 +1,5 @@
 package com.example.project;
 
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,12 +7,11 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectAdapter extends RecyclerView.Adapter<ProjectViewHolder> {
 
-    private List<Employee> employees = new ArrayList<>();
+    private final List<Employee> employees;
     private final ProjectOnClickListener onClickListener;
 
     public ProjectAdapter(List<Employee> employees) {
@@ -33,16 +31,11 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectViewHolder> {
     public void onBindViewHolder(@NonNull ProjectViewHolder holder, int position) {
         holder.getNameView().setText(employees.get(position).getName());
         holder.getPositionView().setText(employees.get(position).getPosition());
-        //holder.variable.setText, Set all values for the list_item
     }
 
     @Override
     public int getItemCount() {
         return employees.size();
-    }
-
-    public void setEmployees(List<Employee> employees) {
-        this.employees = employees;
     }
 
 }
