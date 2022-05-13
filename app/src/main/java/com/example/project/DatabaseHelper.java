794448514cb6f7ed709499ba1776ddde7ad62f36
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-13 15:38:36 +0200

    Add SQLite database code in table and helper class

---
+++ app/src/main/java/com/example/project/DatabaseHelper.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Employee.db";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(DatabaseTable.SQL_CREATE_TABLE_EMPLOYEE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(DatabaseTable.SQL_DELETE_TABLE_EMPLOYEE);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/com/example/project/DatabaseTable.java
@@ -0,0 +1,26 @@
+package com.example.project;
+
+public class DatabaseTable {
+
+    static class SQLEmployee {
+        final static String TABLE_NAME = "employee";
+        final static String COLUMN_NAME = "name";
+        final static String COLUMN_POSITION = "position";
+        final static String COLUMN_LOCATION = "location";
+        final static String COLUMN_ID = "id";
+        final static String COLUMN_DOB = "dob";
+    }
+
+    static final String SQL_CREATE_TABLE_EMPLOYEE =
+            String.format("CREATE TABLE %s (%s INT PRIMARY KEY, %s TEXT, %s TEXT, %s TEXT, %s DATE)",
+                    SQLEmployee.TABLE_NAME,
+                    SQLEmployee.COLUMN_ID,
+                    SQLEmployee.COLUMN_NAME,
+                    SQLEmployee.COLUMN_POSITION,
+                    SQLEmployee.COLUMN_LOCATION,
+                    SQLEmployee.COLUMN_DOB)
+            ;
+
+    static final String SQL_DELETE_TABLE_EMPLOYEE =
+            String.format("DROP TABLE IF EXISTS %s", SQLEmployee.TABLE_NAME);
+}
