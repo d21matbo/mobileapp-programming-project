4cf4628c869b3a4f73d8533f3030c3e20802424e
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-13 17:03:03 +0200

    Add all current fields of Employee.java as columns in DB

--- app/src/main/java/com/example/project/DatabaseTable.java
+++ app/src/main/java/com/example/project/DatabaseTable.java
@@ -4,20 +4,24 @@ public class DatabaseTable {
 
     static class SQLEmployee {
         final static String TABLE_NAME = "employee";
+        final static String COLUMN_ID = "id";
         final static String COLUMN_NAME = "name";
         final static String COLUMN_POSITION = "position";
         final static String COLUMN_LOCATION = "location";
-        final static String COLUMN_ID = "id";
+        final static String COLUMN_NUMBER = "number";
+        final static String COLUMN_MAIL = "mail";
         final static String COLUMN_DOB = "dob";
     }
 
     static final String SQL_CREATE_TABLE_EMPLOYEE =
-            String.format("CREATE TABLE %s (%s INT PRIMARY KEY, %s TEXT, %s TEXT, %s TEXT, %s INT)",
+            String.format("CREATE TABLE %s (%s INT PRIMARY KEY, %s TEXT, %s TEXT, %s TEXT, %s INT, %s TEXT, %s INT)",
                     SQLEmployee.TABLE_NAME,
                     SQLEmployee.COLUMN_ID,
                     SQLEmployee.COLUMN_NAME,
                     SQLEmployee.COLUMN_POSITION,
                     SQLEmployee.COLUMN_LOCATION,
+                    SQLEmployee.COLUMN_NUMBER,
+                    SQLEmployee.COLUMN_MAIL,
                     SQLEmployee.COLUMN_DOB)
             ;
 
